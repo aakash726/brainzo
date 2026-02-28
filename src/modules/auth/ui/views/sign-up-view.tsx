ca482cb1409da204aff1d7a550292b27a76d2cdd
aakash726 <aakashmeena421@gmail.com>
2026-02-28 13:25:55 +0530

    06: dashboard-sidebar

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -215,7 +215,7 @@ export const SignUpView = () => {
                         </div>                
                     </form>
                    </Form>
-                    <div className="bg-radial from-green-500 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <img src="/logo.svg" alt="Image" className="h-[70px] w-[70px]"/>
                         <p className="text-xl font-semibold text-white">Meet.AI</p>
                     </div>
