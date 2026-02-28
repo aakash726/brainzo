0683a27523a9ab8d67f22b6166355ce1a50daf68
aakash726 <aakashmeena421@gmail.com>
2026-02-28 13:52:30 +0530

    correct cn import

--- src/components/generated-avatar.tsx
+++ src/components/generated-avatar.tsx
@@ -2,7 +2,7 @@ import { createAvatar } from "@dicebear/core";
 import { botttsNeutral, initials} from "@dicebear/collection";
 
 
-import { cn } from "@/lib/utils";
+// import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 interface GeneratedAvatarProps {
