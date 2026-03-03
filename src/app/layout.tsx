a401e11c48a6eaa871c4ffb01818cdf19950582a
aakash726 <aakashmeena421@gmail.com>
2026-03-03 16:48:50 +0530

    15: agents filter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next";
+
+import { Toaster } from "@/components/ui/sonner";
 import { TRPCReactProvider } from "@/trpc/client";
 
 import "./globals.css";
@@ -19,14 +22,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-    </TRPCReactProvider>
+    <NuqsAdapter>
+      <TRPCReactProvider>
+        <html lang="en">
+          <body
+            className={`${inter.className} antialiased`}
+          >
+            <Toaster />
+            {children}
+          </body>
+        </html>
+      </TRPCReactProvider>
+    </NuqsAdapter>
   );
 }
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,45 +1,56 @@
 "use client";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
+
 import { useTRPC } from "@/trpc/client";
 import { ErrorState } from "@/components/error-state";
 import { EmptyState } from "@/components/empty-state";
 import { LoadingState } from "@/components/loading-state";
-
 import { columns } from "../components/columns";
 import { DataTable } from "../components/data-table";
+import { DataPagination } from "../components/data-pagination";
+import { useAgentsFilters } from "@/modules/agents/hooks/use-agents-filters";
 
 export const AgentsView = () => {
+  const [filters, setFilters] = useAgentsFilters();
+
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
+    ...filters,
+  }));
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data} columns={columns} />
-      {data.length === 0 && (
+      <DataTable data={data.items} columns={columns} />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
         <EmptyState
           title="Create your first agent"
           description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."
         />
       )}
     </div>
-    )
-}
+  );
+};
 
 export const AgentsViewLoading = () => {
-    return (
-        <LoadingState 
-            title = "Loading Agents"
-            description= "This may take a few seconds"
-        />
-    );
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a fews econds"
+    />
+  );
 };
 
 export const AgentsViewError = () => {
-    return (
-        <ErrorState 
-            title = "Error Loading Agents"
-            description  = "Something went wrong"
-        />
-    )
+  return (
+    <ErrorState
+      title="Error Loading Agents"
+      description="Something went wrong"
+    />
+  )
 }
